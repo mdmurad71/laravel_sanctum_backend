5709d44c34bfaacb2be9b3ef2cacbab6e51fd66c
murad <mdmurad1685@gmail.com>
2023-05-01 14:53:50 -0400

    first commit

---
+++ app/Http/Controllers/Api/AuthController.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
+
+class AuthController extends Controller
+{
+    public function register(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'name' => 'required|max:191',
+            'email' => 'required|email|max:191|unique:users,email',
+            'password' => 'required|min:8'
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json([
+                'validation_errors' => $validator->messages(),
+            ]);
+        } else {
+            $users = User::create([
+                'name' => $request->name,
+                'email' => $request->email,
+                'password' => Hash::make($request->password),
+            ]);
+
+            $token = $users->createToken($users->email . '_token')->plainTextToken;
+
+            return response()->json([
+                'status' => 200,
+                'userName' => $users->name,
+                'token' => $token,
+                'message' => 'resgister successfully',
+            ]);
+        }
+    }
+}
