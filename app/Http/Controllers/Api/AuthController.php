638d4f895075616a3cfc70f6e5d9f39c7dd197dc
murad <mdmurad1685@gmail.com>
2023-05-13 16:48:18 -0400

    change

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -39,4 +39,40 @@ class AuthController extends Controller
             ]);
         }
     }
+
+
+
+    public function Login(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'email' => 'required',
+            'password' => 'required'
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json([
+                'validator_errors' => $validator->message(),
+            ]);
+        } else {
+            $user = User::where('email', $request->email)->first();
+
+            if (!$user || !Hash::check($request->password, $user->password)) {
+                return response()->json([
+                    'status' => '401',
+                    'message' => 'invalid credentials'
+                ]);
+            } else {
+
+
+                $token = $user->createToken($user->email . '_token')->plainTextToken;
+
+                return response()->json([
+                    'status' => 200,
+                    'userName' => $user->name,
+                    'token' => $token,
+                    'message' => 'Loged in successfully',
+                ]);
+            }
+        }
+    }
 }
